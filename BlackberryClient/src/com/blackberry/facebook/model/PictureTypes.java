c92f0aadd155303ad59d2c85b25a26d005e4c206
wtrocki <pantroken@gmail.com>
2012-11-20 19:40:26 +0100

    Update for async requests

---
+++ BlackberryClient/src/com/blackberry/facebook/model/PictureTypes.java
@@ -0,0 +1,11 @@
+package com.blackberry.facebook.model;
+
+public interface PictureTypes {
+	
+	public static final int SMALL = 1;
+	public static final int NORMAL = 2;
+	public static final int LARGE = 3;
+	public static final int SQUARE = 4;
+	public static final int THUMBNAIL = 5;
+	public static final int ALBUM = 6;
+}
